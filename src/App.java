d410274183be965fe977397dfd69fff6c90236d4
EthanJ <ethan.l.jensen@gmail.com>
2026-02-25 17:34:32 +0000

    fdasfdsaf

--- src/App.java
+++ src/App.java
@@ -1,53 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("WorldIndicators2000.csv");
-        Scanner scan = new Scanner(file);
-        int i = 0;
-        scan.nextLine();
-        while(scan.hasNext()){
-            String[] line = scan.nextLine().split(",");
-            if(!line[9].equals("") && !line[12].equals("")){
-            i++;
-            }
-        }
-        scan.close();
-
-        Data[] healthToInfant = new Data[i];
-
-        Scanner scanTwo = new Scanner(file);
-        int j = 0;
-        scanTwo.nextLine();
-        while(scanTwo.hasNext()){
-            String[] line = scanTwo.nextLine().split(",");
-            if(!line[9].equals("") && !line[12].equals("")){
-                healthToInfant[j] = new Data(line[0], Double.parseDouble(line[9]), Double.parseDouble(line[12]));
-                j++;
-            }
-        }
-        scanTwo.close();
-        double totalSpend = 0;
-        for(int k = 0; k < i; k++){
-            totalSpend += healthToInfant[k].getSpend();
-        }
-        double totalInfantMortality = 0;
-        for(int k = 0; k < i; k++){
-            totalInfantMortality += healthToInfant[k].getInfantMortality();
-        }
-        double averageSpend = totalSpend / i;
-        double averageInfantMortality = totalInfantMortality / i;
-        double rTop = 0;
-        double rBottomX = 0;
-        double rBottomY = 0;
-        for(int k = 0; k < i; k++){
-            rTop += ((healthToInfant[k].getSpend() - averageSpend) * (healthToInfant[k].getInfantMortality() - averageInfantMortality));
-            rBottomX += Math.pow((healthToInfant[k].getSpend() - averageSpend), 2);
-            rBottomY += Math.pow((healthToInfant[k].getInfantMortality() - averageInfantMortality), 2);
-        }
-        double r = (rTop / Math.sqrt(rBottomX * rBottomY));
-        System.out.println(r);
+        Data[] healthToInfant = Data.readFile(file);
+        System.out.println(Data.toString(healthToInfant));
     }
 }
--- src/Data.java
+++ src/Data.java
@@ -1,11 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Data {
-    private String name;
     private double spend;
     private double infantMortality;
-    public Data(String dataName, double dataSpend, double dataInfantMortality){
-        name = dataName;
+    public Data(double dataSpend, double dataInfantMortality){
         spend = dataSpend;
         infantMortality = dataInfantMortality;
+    }
+    public static Data[] readFile(File fileName) throws FileNotFoundException {
+        Scanner scanOne = new Scanner(fileName);
+        int i = 0;
+        scanOne.nextLine();
+        while(scanOne.hasNext()){
+            //AI Wrote the Spliter to remove commas in Quotations
+            String[] line = scanOne.nextLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            if(!line[9].equals("") && !line[12].equals("")){
+            i++;
+            }
+        }
+        scanOne.close();
+        Data[] healthToInfant = new Data[i];
+        Scanner scanTwo = new Scanner(fileName);
+        int j = 0;
+        scanTwo.nextLine();
+        while(scanTwo.hasNext()){
+            //AI Wrote the Spliter to remove commas in Quotations
+            String[] line = scanTwo.nextLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            if(!line[9].equals("") && !line[12].equals("")){
+                healthToInfant[j] = new Data(Double.parseDouble(line[9]), Double.parseDouble(line[12]));
+                j++;
+            }
+        }
+        scanTwo.close();
+        return healthToInfant;
     }
     public double getSpend(){
         return spend;
@@ -13,7 +42,56 @@ public class Data {
     public double getInfantMortality(){
         return infantMortality;
     }
-    public String toString(){
-        return "The r value is something";
+    public static double getTotalSpend(Data[] healthToInfant){
+        double totalSpend = 0;
+        for(int i = 0; i < healthToInfant.length; i++){
+            totalSpend += healthToInfant[i].getSpend();
+        }
+        return totalSpend;
+    }
+    public static double getTotalInfantMortality(Data[] healthToInfant){
+        double totalInfantMortality = 0;
+        for(int i = 0; i < healthToInfant.length; i++){
+            totalInfantMortality += healthToInfant[i].getInfantMortality();
+        }
+        return totalInfantMortality;
+    }
+    public static double getR(Data[] healthToInfant){
+        double averageSpend = getTotalSpend(healthToInfant) / healthToInfant.length;
+        double averageInfantMortality = getTotalInfantMortality(healthToInfant) / healthToInfant.length;
+        double rTop = 0;
+        double rBottomX = 0;
+        double rBottomY = 0;
+        for(int i = 0; i < healthToInfant.length; i++){
+            rTop += ((healthToInfant[i].getSpend() - averageSpend) * (healthToInfant[i].getInfantMortality() - averageInfantMortality));
+            rBottomX += Math.pow((healthToInfant[i].getSpend() - averageSpend), 2);
+            rBottomY += Math.pow((healthToInfant[i].getInfantMortality() - averageInfantMortality), 2);
+        }
+        double r = (rTop / Math.sqrt(rBottomX * rBottomY));
+        return r;
+    }
+    public static String toString(Data[] healthToInfant){
+        double r = getR(healthToInfant);
+        String returnString = "------------------------------------------------------------------------------------------------------------------------------------------------------\nThe R Value (Correlation Coefficient) For Your Array Is: " + r + "\n";
+        if(r >= .7){
+            returnString += "Data indicates a strong positive correlation: higher health expenditure as a percentage of GDP is associated with an increased infant mortality rate.";
+        }
+        else if(r >= .3){
+            returnString += "Data indicates a moderate positive correlation: higher health expenditure as a percentage of GDP is associated with an increased infant mortality rate.";
+        }
+        else if(r >= 0){
+            returnString += "Data indicates a weak positive correlation: higher health expenditure as a percentage of GDP is associated with an increased infant mortality rate.";
+        }
+        else if(r > -.3){
+            returnString += "Data indicates a weak negative correlation: higher health expenditure as a percentage of GDP is associated with an decreased infant mortality rate.";
+        }
+        else if(r > -.7){
+            returnString += "Data indicates a moderate negative correlation: higher health expenditure as a percentage of GDP is associated with an decreased infant mortality rate.";
+        }
+        else{
+            returnString += "Data indicates a strong negative correlation: higher health expenditure as a percentage of GDP is associated with an decreased infant mortality rate.";
+        }
+        returnString += "\n------------------------------------------------------------------------------------------------------------------------------------------------------";
+        return returnString;
     }
 }
